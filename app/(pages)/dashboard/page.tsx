3e6b28148dd4632538b076247c33622e14070ead
kennps <napat.samretvit@gmail.com>
2023-08-10 14:17:30 +0700

    Re structure folder

--- app/(pages)/dashboard/page.tsx
+++ app/(pages)/dashboard/page.tsx
@@ -1,7 +1,26 @@
+'use client';
+
+import { useEffect, useMemo } from 'react';
+
 import { ErrorBoundary } from 'react-error-boundary';
 import Number from '@/app/components/number';
+import { useBoundStore } from '@/app/store';
 
 export default function Dashboard() {
+  const fishes = useBoundStore((state) => state.fishes);
+  const [loading, eatFish] = useBoundStore((state) => [
+    state.loading,
+    state.eatFish,
+  ]);
+
+  useEffect(() => {
+    eatFish();
+  }, []);
+
+  useMemo(() => {
+    console.log(loading);
+  }, [loading]);
+
   return (
     <div className="bg-pink-300 w-fit">
       <div>Dashboard</div>
